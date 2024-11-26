929d29d01c45bc3f6e07161dd2e38cafa0d06a0f
Ballesteros <seanjballesteros@gmail.com>
2024-11-26 12:46:00 +0800

    first commit

---
+++ lecture-09-stored-procedures-part-1/jdbc-stored-procedures-tutorial/src/GetCountForDepartment.java
@@ -0,0 +1,67 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+/**
+ * Test calling stored procedure with OUT parameters
+ * 
+ * @author www.luv2code.com
+ *
+ */
+public class GetCountForDepartment {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection myConn = null;
+		CallableStatement myStmt = null;
+
+		try {
+			// Get a connection to database
+			myConn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/demo", "root", "");
+
+			String theDepartment = "Engineering";
+			
+			// Prepare the stored procedure call
+			myStmt = myConn
+					.prepareCall("{call get_count_for_department(?, ?)}");
+
+			// Set the parameters
+			myStmt.setString(1, theDepartment);
+			myStmt.registerOutParameter(2, Types.INTEGER);
+			
+			// Call stored procedure
+			System.out.println("Calling stored procedure.  get_count_for_department('" + theDepartment + "', ?)");
+			myStmt.execute();
+			System.out.println("Finished calling stored procedure");			
+			
+			// Get the value of the OUT parameter
+			int theCount = myStmt.getInt(2);
+			
+			System.out.println("\nThe count = " + theCount);
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			try {
+				close(myConn, myStmt);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void close(Connection myConn, Statement myStmt) throws Exception {
+		if (myStmt != null) {
+			myStmt.close();
+		}
+
+		if (myConn != null) {
+			myConn.close();
+		}
+	}	
+}
